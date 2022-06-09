920031ab179ef10e33badf4ed067f026b6275a2d
nkalinich <nataliekalinich@gmail.com>
2022-06-09 14:07:39 +0300

    test and grade

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // debugger;
 
-
 question_1 = +prompt("Сколько будет 2+2?");
 question_2 = prompt("Солнце встает на востоке?");
-// question_3 = prompt("Сколько будет 5 / 0?");
-// question_4 = prompt("Какого цвета небо?");
-// question_5 = prompt("Какой правильный ответ на главный вопрос жизни, вселенной и всего такого?");
+question_3 = prompt("Сколько будет 5 / 0?");
+question_4 = prompt("Какого цвета небо?");
+question_5 = +prompt("Какой правильный ответ на главный вопрос жизни, вселенной и всего такого?");
 
 x = 0;
 
@@ -19,11 +18,34 @@ if (!question_1) {
 
 if (!question_2) {
     console.log (x);
-} else if (question_1 === "Да") {
+} else if (question_2 === "Да") {
     console.log (x += 10);
 } else {
     console.log(x);
 }
 
+if (!question_3) {
+    console.log (x);
+} else if (question_3 === "Делить на 0 нельзя") {
+    console.log (x += 10);
+} else {
+    console.log(x);
+}
+
+if (!question_4) {
+    console.log (x);
+} else if (question_4 === "Голубое") {
+    console.log (x += 10);
+} else {
+    console.log(x);
+}
+
+if (!question_5) {
+    console.log (x);
+} else if (question_5 === 42) {
+    console.log (x += 10);
+} else {
+    console.log(x);
+}
 
 alert(`Ваш результат: ${x}`);
